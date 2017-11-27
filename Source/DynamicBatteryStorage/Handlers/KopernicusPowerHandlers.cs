9a589dcf9db126db37921713060999feced4dc0d
Chris Adderley <cadderley@gmail.com>
2017-11-27 07:51:41 -0800

    Solar panel fix

---
+++ Source/DynamicBatteryStorage/Handlers/KopernicusPowerHandlers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+
+namespace DynamicBatteryStorage
+{
+
+    // Special power handler for Kopernicus's replaced panels
+    public class KopernicusSolarPanelHandler: PowerHandler
+    {
+      ModuleDeployableSolarPanel panel;
+
+      public override void Initialize(PartModule pm)
+      {
+          base.Initialize(pm);
+        panel = (ModuleDeployableSolarPanel)pm;
+      }
+
+      public override double GetPower()
+      {
+        if (panel != null)
+          return (double)panel.flowRate;
+        return 0d;
+      }
+    }
+    
+}
