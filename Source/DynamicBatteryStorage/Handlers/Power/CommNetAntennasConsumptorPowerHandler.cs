9f52fbdd0a02646fd53c3e0e77b10c7f1d85c210
Alexander Yalov <alexander.yalov@gmail.com>
2021-07-04 21:11:49 +0300

    ModuleGeneratorAntenna support (CommNetAntennasConsumptor mod)

---
+++ Source/DynamicBatteryStorage/Handlers/Power/CommNetAntennasConsumptorPowerHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicBatteryStorage
+{
+    public class ModuleGeneratorAntennaPowerHandler : ModuleGeneratorPowerHandler
+    {
+        public ModuleGeneratorAntennaPowerHandler(HandlerModuleData moduleData) : base(moduleData)
+        { }
+    }
+}
